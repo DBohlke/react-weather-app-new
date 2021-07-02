06e4f9509ba3f554185ba879c801a19c880ca26f
DBohlke <78039823+DBohlke@users.noreply.github.com>
2021-07-01 23:26:57 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Dani Bohlke and is open-sourced on{" "}
+        <a
+          href="https://github.com/DBohlke/react-weather-app-new"
+          target="_blank"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
