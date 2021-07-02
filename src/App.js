cf75552cf96adaba9de482d3658f23d872948d3a
DBohlke <78039823+DBohlke@users.noreply.github.com>
2021-07-02 00:03:05 -0500

    Updated GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
         <a
           href="https://github.com/DBohlke/react-weather-app-new"
           target="_blank"
+          rel="noopener noreferrer"
         >
           Github
         </a>
