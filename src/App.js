5fcf4d47cc0474849bf79ad8b3babd1c0c2d3ad7
DBohlke <78039823+DBohlke@users.noreply.github.com>
2021-07-02 23:15:39 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Dani Bohlke and is open-sourced on{" "}
-        <a
-          href="https://github.com/DBohlke/react-weather-app-new"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Dani Bohlke and is open-sourced on{" "}
+          <a
+            href="https://github.com/DBohlke/react-weather-app-new"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
